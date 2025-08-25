369fa2e98dc48286c8efa1ddf39d447130f44038
NhatHM4 <haminhnhat711@gmail.com>
2025-08-25 15:56:04 +0700

    config with serve

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,8 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: "react-cicd",
-      script: "npm run preview -- --host",
+      name: 'react-vite-app',
+      script: "serve",
+      env: {
+        PM2_SERVE_PATH: './dist',  // Trỏ đến thư mục dist
+        PM2_SERVE_PORT: 3001,
+        PM2_SERVE_SPA: 'true',     // Cho React SPA
+        PM2_SERVE_HOMEPAGE: '/index.html'
+      }
     }
   ]
-}
+};
